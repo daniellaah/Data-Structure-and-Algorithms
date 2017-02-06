6d21967e517effd1e56976d133e5dc72d773e0ba
daniellaah <bobogao223@163.com>
2017-02-07 03:56:26 +0800

    update

--- Beauty-of-Programming/2.1求二进制数中1的个数.py
+++ Beauty-of-Programming/2.1求二进制数中1的个数.py
@@ -1,9 +1,11 @@
+# 解法一
 def countBit(num):
     count = 0
     while num > 0:
         count += 1
         num &= num - 1
     return count
+#解法二 当输入较小时, 例如一个8bits的无符号整型数据, 可以直接使用'查表法'
 
 if __name__ == "__main__":
     num = 100
