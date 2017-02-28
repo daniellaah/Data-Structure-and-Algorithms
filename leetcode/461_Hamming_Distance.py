17afaaa0e67d33793dc8e3c49f6e26c6b818d8de
daniellaah <bobogao223@163.com>
2017-03-01 06:17:23 +0800

    add 461_Hamming_Distance.py

---
+++ leetcode/461_Hamming_Distance.py
@@ -0,0 +1,35 @@
+"""关键: 统计二进制数中1的个数
+461. Hamming Distance
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different."""
+
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        xor = x ^ y
+        count = 0
+        while xor > 0:
+            xor &= xor - 1
+            count += 1
+        return count
