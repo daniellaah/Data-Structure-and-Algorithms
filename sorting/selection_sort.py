c9ba9ab278fe8753b92994fc66742e906fe2778a
daniellaah <bobogao223@163.com>
2017-03-17 19:26:15 +0800

    modify selection sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,17 +1,19 @@
 from random_nums import random_nums
 
-def insertion_sort(nums):
-    nums_len = len(nums)
-    for i in range(nums_len):
-        max_index = 0
-        for j in range(nums_len - i):
-            if nums[max_index] < nums[j]:
-                max_index = j
-        nums[max_index], nums[j] = nums[j], nums[max_index]
+# time complexity: O(n^2)
+def selection_sort(nums):
+    nums_length = len(nums)
+    for i in range(nums_length):
+        max_idx = 0
+        for j in range(nums_length - i):
+            if nums[max_idx] < nums[j]:
+                max_idx = j
+        nums[max_idx], nums[j] = nums[j], nums[max_idx]
     return nums
+
 
 if __name__ == "__main__":
     nums = random_nums()
     print(nums)
-    insertion_sort(nums)
+    selection_sort(nums)
     print(nums)
