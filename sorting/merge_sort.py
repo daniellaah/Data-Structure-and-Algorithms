f5f198811685d7ecf78a9f12ca90223fbbe618fe
daniellaah <bobogao223@163.com>
2017-03-17 22:00:08 +0800

    update merge sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,20 +1,16 @@
-from random_nums import random_nums
-
+from random import randint
 def merge(nums, start, mid, end):
     tmp = []
     i = start
     j = mid
     while i < mid and j < end:
-        if nums[i] < nums[j]:
+        if nums[i] <= nums[j]:
             tmp.append(nums[i])
             i += 1
         elif nums[j] < nums[i]:
             tmp.append(nums[j])
             j += 1
-        else:
-            tmp += [nums[i], nums[j]]
-            i += 1
-            j += 1
+
     if i == mid and j < end:
         tmp += nums[j:end]
     elif i < mid and j == end:
@@ -25,15 +21,13 @@ def merge(nums, start, mid, end):
 
 def merge_sort(nums, start, end):
     mid = (end - start) // 2 + start
-    if end - start > 2:
+    if end - start >= 2:
         merge_sort(nums, start, mid)
         merge_sort(nums, mid, end)
         merge(nums, start, mid, end)
-    else:
-        merge(nums, start, mid, end)
 
 if __name__ == "__main__":
-    nums = random_nums()
+    nums = [randint(1,100) for _ in range(10)]
     print(nums)
     merge_sort(nums, 0, len(nums))
     print (nums)
