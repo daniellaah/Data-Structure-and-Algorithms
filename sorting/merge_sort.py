4aa8122fb688bb9f923683d5ff2b5104f6a1e658
daniellaah <bobogao223@163.com>
2017-03-20 02:30:09 +0800

    add sorting

---
+++ sorting/merge_sort.py
@@ -0,0 +1,33 @@
+from random import randint
+def merge(nums, start, mid, end):
+    tmp = []
+    i = start
+    j = mid
+    while i < mid and j < end:
+        if nums[i] <= nums[j]:
+            tmp.append(nums[i])
+            i += 1
+        elif nums[j] < nums[i]:
+            tmp.append(nums[j])
+            j += 1
+
+    if i == mid and j < end:
+        tmp += nums[j:end]
+    elif i < mid and j == end:
+        tmp += nums[i:mid]
+
+    for i in range(start, end):
+        nums[i] = tmp[i - start]
+
+def merge_sort(nums, start, end):
+    mid = (end - start) // 2 + start
+    if end - start >= 2:
+        merge_sort(nums, start, mid)
+        merge_sort(nums, mid, end)
+        merge(nums, start, mid, end)
+
+if __name__ == "__main__":
+    nums = [randint(1,100) for _ in range(10)]
+    print(nums)
+    merge_sort(nums, 0, len(nums))
+    print (nums)
---
+++ sorting/selection_sort.py
@@ -0,0 +1,20 @@
+from random_nums import random_nums
+
+# time complexity: O(n^2)
+# 循环遍历数组, 每次找到最大的放数放到最后
+def selection_sort(nums):
+    nums_length = len(nums)
+    for i in range(nums_length):
+        max_idx = 0
+        for j in range(nums_length - i):
+            if nums[max_idx] < nums[j]:
+                max_idx = j
+        nums[max_idx], nums[j] = nums[j], nums[max_idx]
+    return nums
+
+
+if __name__ == "__main__":
+    nums = random_nums()
+    print(nums)
+    selection_sort(nums)
+    print(nums)
