8c62929b7fc388d1a55a1763707871ae8d012102
daniellaah <bobogao223@163.com>
2017-03-20 00:01:24 +0800

    add 144. Binary Tree Preorder Traversal

---
+++ leetcode/144_Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,35 @@
+"""144. Binary Tree Preorder Traversal
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return
+        ret, stack = [root], []
+        while stack:
+            node = stack.pop()
+            if node:
+                ret.append(node)
+                stack.append(node.right)
+                stack.append(node.left)
+        return ret
