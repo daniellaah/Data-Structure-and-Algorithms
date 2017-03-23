dc7324f2fd0397ae65476c4292761af053f3f3c3
daniellaah <bobogao223@163.com>
2017-03-23 21:53:17 +0800

    add 110. Balanced Binary Tree

---
+++ leetcode/110_Balanced_Binary_Tree.py
@@ -0,0 +1,28 @@
+"""110. Balanced Binary Tree
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def getDepth(self, root):
+        if not root:
+            return 0
+        return max(self.getDepth(root.left), self.getDepth(root.right)) + 1
+
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        if abs(self.getDepth(root.left) - self.getDepth(root.right)) > 1:
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
