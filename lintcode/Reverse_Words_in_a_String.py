a06da06558419507132f3ce3e50c7e3d6aef185d
daniellaah <bobogao223@163.com>
2017-03-24 02:16:58 +0800

    add Reverse Words in a String

---
+++ lintcode/Reverse_Words_in_a_String.py
@@ -0,0 +1,20 @@
+"""Reverse Words in a String
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+Clarification
+What constitutes a word?
+A sequence of non-space characters constitutes a word.
+Could the input string contain leading or trailing spaces?
+Yes. However, your reversed string should not contain leading or trailing spaces.
+How about multiple spaces between two words?
+Reduce them to a single space in the reversed string.
+"""
+class Solution:
+    # @param s : A string
+    # @return : A string
+    def reverseWords(self, s):
+        # write your code here
+        return " ".join(s.split()[::-1])
