bada30f4b61452d78cbf0be1792afca5bff1422f
daniellaah <bobogao223@163.com>
2017-03-28 22:24:40 +0800

    update 144. Binary Tree Preorder Traversal

--- leetcode/144_Binary_Tree_Preorder_Traversal.py
+++ leetcode/144_Binary_Tree_Preorder_Traversal.py
@@ -24,12 +24,13 @@ class Solution(object):
         :rtype: List[int]
         """
         if not root:
-            return
-        ret, stack = [root], []
+            return []
+        ret, stack = [], [root]
         while stack:
             node = stack.pop()
             if node:
-                ret.append(node)
+                ret.append(node.val)
                 stack.append(node.right)
                 stack.append(node.left)
         return ret
+        
