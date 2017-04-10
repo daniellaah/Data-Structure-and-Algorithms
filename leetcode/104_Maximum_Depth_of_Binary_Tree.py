8df6e27adcb6b5863758070758245067c299802c
daniellaah <bobogao223@163.com>
2017-04-10 18:51:02 +0800

    update 104

--- leetcode/104_Maximum_Depth_of_Binary_Tree.py
+++ leetcode/104_Maximum_Depth_of_Binary_Tree.py
@@ -9,7 +9,7 @@ The maximum depth is the number of nodes along the longest path from the root no
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+# 解法一, 递归DFS
 class Solution(object):
     def maxDepth(self, root):
         """
@@ -19,3 +19,32 @@ class Solution(object):
         if not root:
             return 0
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+# 解法二, 循环BFS
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        queue = [root]
+        depth = 0
+        while queue:
+            depth += 1
+            bfs = len(queue)
+            while bfs > 0:
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                bfs -= 1
+        return depth
