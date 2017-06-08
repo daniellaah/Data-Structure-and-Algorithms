1d2b0362802acb09e95408c8147ce632d1d60496
daniellaah <bobogao223@163.com>
2017-06-08 22:02:16 +0800

    update bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,16 +1,15 @@
-from random_nums import random_nums
+from random import randint
 # time complexity: O(n^2)
 # 循环遍历数组, 相邻两个比较, 交换顺序
-def bubble_sort(nuns):
-    nums_length = len(nums)
-    for i in range(nums_length - 1):
-        for j in range(nums_length - 1 - i):
+def bubble_sort(nums):
+    nums_len = len(nums)
+    for i in range(1, nums_len):
+        for j in range(nums_len - i):
             if nums[j] > nums[j+1]:
                 nums[j], nums[j+1] = nums[j+1], nums[j]
-    return nums
 
 if __name__ == "__main__":
-    nums = random_nums()
+    nums = [randint(0, 100) for _ in range(10)]
     print(nums)
     bubble_sort(nums)
     print(nums)
