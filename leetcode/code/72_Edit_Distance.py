3de0d2aa34b5cc43a3f25eb5db9da6abde72eeb4
daniellaah <bobogao223@163.com>
2017-06-18 14:19:04 +0800

    add 72 edit distance

---
+++ leetcode/code/72_Edit_Distance.py
@@ -0,0 +1,31 @@
+'''72. Edit Distance
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+'''
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        if word1 == word2:
+            return 0
+        len_word1, len_word2 = len(word1), len(word2)
+        D = [[0 for _ in range(len_word2+1)] for _ in range(len_word1+1)]
+        for i in range(len_word1+1):
+            D[i][0] = i
+        for j in range(len_word2+1):
+            D[0][j] = j
+        for i in range(1, len_word1+1):
+            for j in range(1, len_word2+1):
+                D[i][j] = min(D[i-1][j] + 1,
+                              D[i][j-1] + 1,
+                              D[i-1][j-1] if word1[i-1] == word2[j-1] else
+                              D[i-1][j-1] + 1)
+        return D[len_word1][len_word2]
